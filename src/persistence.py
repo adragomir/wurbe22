0e686d5f570a34a3917d5ed028010d5346901828
Andrei Dragomir <adragomir@gmail.com>
2009-07-04 16:13:35 +0300

    create indexes

--- src/persistence.py
+++ src/persistence.py
@@ -4,9 +4,18 @@ class MongoPersistence(object):
     def __init__(self):
         self.connection = pymongo.connection.Connection()
         self.db = self.connection.underpants
+        self.entries = self.db.entries
+        self.create_indexes()
+
+    def create_indexes(self):
+        self.entries.create_index("calaised", pymongo.ASCENDING)
+        self.entries.create_index("published", pymongo.ASCENDING)
+        self.entries.create_index("updated", pymongo.ASCENDING)
 
     def add(self, obj):
-        print "adding"
+        print "Saving entry...."
+        self.db.entries.save(obj)
+
+    def find(conditions):
         pass
-        
 
