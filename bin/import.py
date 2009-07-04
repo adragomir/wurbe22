57b911cb183544a35b1e1f44e3f41e80bda8d449
Andrei Dragomir <adragomir@gmail.com>
2009-07-04 16:13:21 +0300

    implemented file import (json format) for friendfeed

--- bin/import.py
+++ bin/import.py
@@ -4,7 +4,7 @@ import sys
 import config
 import ingest
 from ingesters import friendfeed
-from util import ff
+from util import ff, json
 import persistence
 
 # TODO: de-hardcode friendfeed import
@@ -12,8 +12,11 @@ def import_main(options, args):
     if options.flavor == 'friendfeed':
         i = ingest.Ingester(persistence.MongoPersistence())
         flavor_object = friendfeed.FriendFeedInput()
-        f = ff.FriendFeedImport(config.friendfeed['username'], config.friendfeed['password'])
-        result = f.get_all_home_entries()
+        if options.file is not None:
+            result = json.loads(open(options.file, "rb+").read())
+        else:
+            f = ff.FriendFeedImport(config.friendfeed['username'], config.friendfeed['password'])
+            result = f.get_all_home_entries()
         flavor_object.data = result
         i.ingest(flavor_object)
     else:
@@ -21,10 +24,15 @@ def import_main(options, args):
 
 if __name__ == "__main__":
 
-    parser = optparse.OptionParser("usage: %ingest flavor [args]")
-    parser.add_option("-f", "--flavor", dest="flavor",
+    parser = optparse.OptionParser("usage: %ingest [args]")
+    parser.add_option("-w", "--where", dest="flavor",
                   default="friendfeed", type="string",
                   help="where to import the data from")
+    parser.add_option("-f", "--file", dest="file",
+                  default=None, type="string",
+                  help="File for input")
     (options, args) = parser.parse_args()
+    print repr(args)
+    print repr(options)
     import_main(options, args)
 
